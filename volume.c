94fd29456b3534bfbe38eeaf8dc28b3cde1ab357
Banduliak Andrii <109424920+AndriiBanduliak@users.noreply.github.com>
2023-06-19 22:19:51 +0300

    week # 4

---
+++ volume.c
@@ -0,0 +1,51 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "rb");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "wb");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    // Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
+    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
+
+    // Read samples from input file and write updated data to output file
+    int16_t sample;
+    while (fread(&sample, sizeof(int16_t), 1, input))
+    {
+        sample = sample * factor;
+        fwrite(&sample, sizeof(int16_t), 1, output);
+    }
+
+    // Close files
+    fclose(input);
+    fclose(output);
+
+    return 0;
+}
